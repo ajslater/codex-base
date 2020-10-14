4a165bd5e0c8bbcbf6f5b12e2dadad1bfd8d047f
AJ Slater <aj@slater.net>
2020-10-13 20:04:31 -0700

    v0.1.2 (#3)
    * initial commit
    
    * fix base image name
    
    * fix tag name
    
    * bump version to fix tagname

--- ci/tag-and-push.sh
+++ ci/tag-and-push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
+# shellcheck disable=SC1091
 source ./env
-VERSION=python${BASE_VERSION}_${PKG_VERSION}
+VERSION=${BASE_VERSION}_${PKG_VERSION}
 TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
 docker tag "$IMAGE" "$IMAGE:$TAG"
 docker tag "$IMAGE" "$IMAGE:latest"
